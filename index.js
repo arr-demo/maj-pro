ae08c3f2f8058ca39f3835fc6dbc278df2ccd4ec
Abdul Rub Rayyan <abdulrubrayyan@gmail.com>
2019-10-01 15:38:48 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express()
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
+const publicPath = path.join(__dirname, './public')
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
+app.use(express.static(publicPath))
+
+
+//Homepage
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//Sign Up page
+app.get('/register', (req, res) => {
+    res.send('Hello, this is the Sign Up page')
+})
+
+//Login Page
+app.get('/login', (req, res) => {
+    res.send('Hello, this is the Login Page')
+})
+
+//Leaderboard Page
+app.get('/ranks', (req, res) => {
+    res.send('Hello, this is the Leaderboard page')
+})
+
+//Problem page
+app.get('/problem/:id', (req, res) => {
+    res.send('Hello, this is the Problem page.')
+})
+
+app.listen(port, () => {
+    console.log('Listening to '+port);
+})
+
